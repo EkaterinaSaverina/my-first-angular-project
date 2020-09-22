3d1602ba07066174b5b18f8e33dcdda03af4c759
EkaterinaSaverina <ekaterina.saverina@hiqo-solutions.com>
2020-09-22 12:43:55 +0300

    HEDU-4901: stabilize the AuthGuard work

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
-import { isUnderfined } from 'lodash';
+import { isBoolean } from 'lodash';
 
 import { UsersService } from '../services';
 
@@ -22,7 +22,7 @@ export class AuthGuard implements CanActivate {
     return this.usersService.isAuthorized()
       .pipe(
         filter(isAuth => {
-          return !isUnderfined(isAuth);
+          return isBoolean(isAuth);
         }),
         map(isAuth => {
           if (!isAuth) {
