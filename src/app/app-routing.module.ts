6a585f13cf621c0fecc51c35628529116c2730b5
EkaterinaSaverina <ekaterina.saverina@hiqo-solutions.com>
2020-09-23 17:12:16 +0300

    HEDU-4901: Delete unnecessary barrel imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthGuard } from './core/guards/auth.guard';
-import { PageNotFoundComponent } from './page-not-found';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
