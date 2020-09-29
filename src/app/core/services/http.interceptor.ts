951c6f449bc59e204d306c9b02dca01eb218a2bc
EkaterinaSaverina <ekaterina.saverina@hiqo-solutions.com>
2020-09-29 19:21:47 +0300

    HEDU-4901: add correct indents for interceptor.ts

--- src/app/core/services/http.interceptor.ts
+++ src/app/core/services/http.interceptor.ts
@@ -7,23 +7,23 @@ import { tap } from 'rxjs/operators';
 import { SpinnerService } from './spinner.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AppHttpInterceptor implements HttpInterceptor {
 
-    constructor(private spinner: SpinnerService) { }
+  constructor(private spinner: SpinnerService) { }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        this.spinner.show();
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.spinner.show();
 
-        return next.handle(req)
-            .pipe(
-                tap(
-                    data => {
-                        console.log(data);
-                        this.spinner.hide();
-                    }
-                )
-            );
-    }
+    return next.handle(req)
+      .pipe(
+        tap(
+          data => {
+            console.log(data);
+            this.spinner.hide();
+          }
+        )
+      );
+  }
 }
